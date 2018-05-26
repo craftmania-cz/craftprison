d61aa3ed427f47c7dabbde9076aa55c0f48ff085
MrWakeCZ <joseff.kral@gmail.com>
2018-05-26 17:30:32 +0200

    Oprava actionbaru na spawnu

--- src/main/java/cz/wake/craftprison/modules/ActionBarProgress.java
+++ src/main/java/cz/wake/craftprison/modules/ActionBarProgress.java
@@ -12,7 +12,7 @@ public class ActionBarProgress {
 
     public static void send() {
         for (Player p : Bukkit.getOnlinePlayers()) {
-            if (p.getLocation().getWorld().getName().equals("doly")) {
+            if (p.getLocation().getWorld().getName().equals("doly") || p.getLocation().getWorld().getName().equals("spawn")) {
                 CraftPlayer cp = pm.getCraftPlayer(p);
                 new ActionBar(getText(Main.getEconomy().getBalance(p), cp.getRank().getNext().getPrice()), -1).send(p);
             }
