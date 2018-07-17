e9c7c5f5eb7c72cdf27bbb751eeab7e08d339bcb
MrWakeCZ <joseff.kral@gmail.com>
2018-07-17 02:12:55 +0200

    Kontrola zda má hráč v inventáři obsidian při vstupu do /pvp

--- src/main/java/cz/wake/craftprison/commands/PVPCommand.java
+++ src/main/java/cz/wake/craftprison/commands/PVPCommand.java
@@ -2,6 +2,7 @@ package cz.wake.craftprison.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +16,10 @@ public class PVPCommand implements CommandExecutor {
             Player player = (Player) sender;
             if ((command.getName().equalsIgnoreCase("pvp"))) {
                 if(player.hasPermission("craftprison.pvp")) {
+                    if(player.getInventory().contains(Material.OBSIDIAN)) {
+                        player.sendMessage("§c§l(!) §cObsidian si musis natezit v dole, ne si ho tam nosit jak na dovolenou!");
+                        return false;
+                    }
                     player.teleport(new Location(Bukkit.getWorld("doly"), -1078.5, 118, 1374.5, -89, 0));
                 } else {
                     player.sendMessage("§c§l(!) §cK ziskani pristupu do PVP musis mit rank §bBender§c.");
