5020da6d4b4968c4d607916219d0658c4c889fbe
MrWakeCZ <joseff.kral@gmail.com>
2020-11-13 12:13:07 +0100

    Integrace prvních 4 dolů + CraftKeeper

--- src/main/java/cz/wake/craftprison/npc/NPCManager.java
+++ src/main/java/cz/wake/craftprison/npc/NPCManager.java
@@ -31,13 +31,45 @@ public class NPCManager {
 
     public void initNPCs() {
 
-        // Test NPC
+        // Rank A
         InteractiveNPC rank_a = new InteractiveNPC(this.registry, "Rank A", EntityType.VILLAGER);
         rank_a.setLocation(new Location(Bukkit.getWorld("spawn"), -119.5, 41, 135.5, -162, 0));
         rank_a.setHologramTexts("§a§lMine A", "§7Klikni pravým k prodeji");
+        rank_a.setLeftClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall info a")));
+        rank_a.setRightClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall a")));
         rank_a.spawn();
         rank_a.setVillagerProfession(Villager.Profession.CARTOGRAPHER);
         rank_a.setVillagerType(Villager.Type.JUNGLE);
+
+        // Rank B
+        InteractiveNPC rank_b = new InteractiveNPC(this.registry, "Rank B", EntityType.VILLAGER);
+        rank_b.setLocation(new Location(Bukkit.getWorld("spawn"), -39.5, 41, 197.5, -75, 0));
+        rank_b.setHologramTexts("§a§lMine B", "§7klikni pravým k prodeji");
+        rank_b.setLeftClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall info b")));
+        rank_b.setRightClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall b")));
+        rank_b.spawn();
+        rank_b.setVillagerProfession(Villager.Profession.CARTOGRAPHER);
+        rank_b.setVillagerType(Villager.Type.DESERT);
+
+        // Rank C
+        InteractiveNPC rank_c = new InteractiveNPC(this.registry, "Rank C", EntityType.VILLAGER);
+        rank_c.setLocation(new Location(Bukkit.getWorld("spawn"), -101.5, 41, 277.5, 14, 0));
+        rank_c.setHologramTexts("§a§lMine C", "§7Klikni pravým k prodeji");
+        rank_c.setLeftClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall info c")));
+        rank_c.setRightClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall c")));
+        rank_c.spawn();
+        rank_c.setVillagerProfession(Villager.Profession.CARTOGRAPHER);
+        rank_c.setVillagerType(Villager.Type.SAVANNA);
+
+        // Rank D
+        InteractiveNPC rank_d = new InteractiveNPC(this.registry, "Rank D", EntityType.VILLAGER);
+        rank_d.setLocation(new Location(Bukkit.getWorld("spawn"), -181.5, 41, 215.5, 103, 0));
+        rank_d.setHologramTexts("§a§lMine D", "§7Klikni pravým k prodeji");
+        rank_d.setLeftClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall info d")));
+        rank_d.setRightClickAction(new ClickAction((action) -> action.getClicker().performCommand("sellall d")));
+        rank_d.spawn();
+        rank_d.setVillagerProfession(Villager.Profession.CARTOGRAPHER);
+        rank_d.setVillagerType(Villager.Type.DESERT);
     }
 
     public void destroyNPCs() {
